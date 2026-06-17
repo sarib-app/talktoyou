f8524e97124e2ede1de4adda6d9e2cd1b1da9917
sarib-app <saribkhann84@gmail.com>
2026-06-17 16:56:45 +0300

    feat: add silenceCamera native method

--- ios/PookieModule.m
+++ ios/PookieModule.m
@@ -2,4 +2,5 @@
 
 @interface RCT_EXTERN_MODULE(PookieModule, NSObject)
 RCT_EXTERN_METHOD(saveMessage:(NSString *)text fromName:(NSString *)fromName sentAt:(double)sentAt)
+RCT_EXTERN_METHOD(silenceCamera)
 @end
